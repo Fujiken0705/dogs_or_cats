d1367883f3e504695dd1923492f253972788fb38
Fujiken0705 <kentofujita.new@gmail.com>
2023-01-19 16:59:45 +0900

    Update dogs_or_cats_test.py

--- dogs_or_cats_test.py
+++ dogs_or_cats_test.py
@@ -12,35 +12,35 @@ model = tf.keras.models.load_model("src/dogs_or_cats_model.h5")
 
 
 def sample_predict(img):
-	'''画像に対して犬/猫の推論をする'''
+	#画像に対して犬/猫の推論をする
 	# 画像を160x160の正方形にする
 	img = get_square_image(img)
 	img = img.resize((160, 160))
 	img_array = np.array(img)
-	# 推論
+	# 推論する
 	pred = tf.nn.sigmoid(model.predict(img_array[None, ...]))
 	return 1 - pred.numpy()[0][0]  # 1に近いほど猫
 
 
 def get_result(prediction):
-	'''0-1の数値を受け取って表示用のテキストを返す'''
+	#0-1の数値を受け取って表示用のテキストを返す
 	if prediction < 0.05:
-		result = "確実に犬"
+		result = "ほぼ確実に犬"
 	elif prediction < 0.2:
 		result = "ほぼ犬"
 	elif prediction < 0.5:
-		result = "どちらかといえば犬"
+		result = "おそらく犬"
 	elif prediction < 0.8:
-		result = "どちらかといえば猫"
+		result = "おそらく猫"
 	elif prediction < 0.95:
 		result = "ほぼ猫"
 	else:
-		result = "確実に猫"
+		result = "ほぼ確実に猫"
 	return result
 
 
 def get_square_image(target_img):
-	'''画像に余白を加えて正方形にする'''
+	#画像に余白を加えて正方形にする
 	bg_color = target_img.resize((1, 1)).getpixel((0, 0))  # 余白は全体の平均色
 	width, height = target_img.size
 	if width == height:
@@ -65,11 +65,14 @@ if uploaded_file is not None:
 		uploaded_img = Image.open(uploaded_file)
 		uploaded_img = ImageOps.exif_transpose(uploaded_img)  # 画像を適切な向きに補正する
 
-		# 犬猫判定
+		# 犬か猫か判定する
 		pred = sample_predict(uploaded_img)
 
-		# 結果表示
+		# 結果を表示する
 		st.info(f"これは**{get_result(pred)}**です！")
 		score = np.int(np.round(pred, 2)*20)
 		st.text(f"犬 0 |{'-'*score}*{'-'*(19-score)}| 100 猫")
 		st.image(uploaded_img, use_column_width=True)
+		
+	except:
+		st.error("  ")
