3dd12e3259529c3076ddcdb2bc86cc4d887de60c
Fujiken0705 <kentofujita.new@gmail.com>
2023-01-19 16:35:22 +0900

    Update dogs_or_cats_test.py

--- dogs_or_cats_test.py
+++ dogs_or_cats_test.py
@@ -7,23 +7,23 @@ import tensorflow as tf
 from PIL import Image, ImageOps
 
 
-### モデル部分
+# TensorFlowで作成したモデルを導入する
 model = tf.keras.models.load_model("src/dogs_or_cats_model.h5")
 
 
 def sample_predict(img):
-	'''画像に対して犬/猫の推論をする'''
+	#画像に対して犬/猫の推論をする
 	# 画像を160x160の正方形にする
 	img = get_square_image(img)
 	img = img.resize((160, 160))
 	img_array = np.array(img)
-	# 推論
+	# 推論する
 	pred = tf.nn.sigmoid(model.predict(img_array[None, ...]))
 	return 1 - pred.numpy()[0][0]  # 1に近いほど猫
 
 
 def get_result(prediction):
-	'''0-1の数値を受け取って表示用のテキストを返す'''
+	# 0-1の数値を受け取って表示用のテキストを返す
 	if prediction < 0.05:
 		result = "確実に犬"
 	elif prediction < 0.2:
@@ -40,8 +40,8 @@ def get_result(prediction):
 
 
 def get_square_image(target_img):
-	'''画像に余白を加えて正方形にする'''
-	bg_color = target_img.resize((1, 1)).getpixel((0, 0))  # 余白は全体の平均色
+	# 画像に余白を加えて正方形にする
+	bg_color = target_img.resize((1, 1)).getpixel((0, 0))
 	width, height = target_img.size
 	if width == height:
 		return target_img
@@ -56,22 +56,19 @@ def get_square_image(target_img):
 
 
 ### 表示部分
-st.title("cat or dog ?")
+st.title("犬猫判別アプリ")
 
-uploaded_file = st.file_uploader("判定したい画像を選んでね")
+uploaded_file = st.file_uploader("判定したい画像をアップロードしてください")
 if uploaded_file is not None:
-	try:
-		# 画像を読み込む
-		uploaded_img = Image.open(uploaded_file)
-		uploaded_img = ImageOps.exif_transpose(uploaded_img)  # 画像を適切な向きに補正する
+	# 画像を読み込む
+	uploaded_img = Image.open(uploaded_file)
+	uploaded_img = ImageOps.exif_transpose(uploaded_img)  # 画像を適切な向きに補正する
 
-		# 犬猫判定
-		pred = sample_predict(uploaded_img)
+	# 犬猫判定
+	pred = sample_predict(uploaded_img)
 
-		# 結果表示
-		st.info(f"これは**{get_result(pred)}**です！")
-		score = np.int(np.round(pred, 2)*20)
-		st.text(f"犬 0 |{'-'*score}*{'-'*(19-score)}| 100 猫")
-		st.image(uploaded_img, use_column_width=True)
-	except:
-		st.error("判定できませんでした・・・適切な画像をアップロードしてください！")
+	# 結果表示
+	st.info(f"これは**{get_result(pred)}**です！")
+	score = np.int(np.round(pred, 2)*20)
+	st.text(f"犬 0 |{'-'*score}*{'-'*(19-score)}| 100 猫")
+	st.image(uploaded_img, use_column_width=True)
